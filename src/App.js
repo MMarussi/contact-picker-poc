4aef587e9a8821aff63b90531444b7aa55e5595b
MMarussi <jmmarussi32@gmail.com>
2020-04-21 17:25:46 -0300

    Added button for contact gathering

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Snackbar from './components/snackbar';
 function App() {
   const [isOnLine, setOnline] = useState(navigator.onLine);
   const updateNetworkStatus = () => setOnline(navigator.onLine);
+  const [contacts, setContacts] = useState([]);
 
   useEffect(() => {
     window.addEventListener('offline', updateNetworkStatus);
@@ -18,35 +19,40 @@ function App() {
     };
   });
 
-  const contactsSupported = ('contacts' in navigator && 'ContactsManager' in window);
+  const contactsSupported = 'contacts' in navigator && 'ContactsManager' in window;
 
-  console.log(window.isSecureContext);
-  console.log(navigator);
-
-  const renderContacts = async () => {
-    try {
-      const contacts = await navigator.contacts.select(['name'], {});
-      contacts.forEach(contact => console.log(contact));
-    } catch (error) {
-      console.log(error);
-    }
+  const gatherContacts = () => {
+    navigator.contacts
+      .select(['name'], {})
+      .then((response) => {
+        console.log('Contacts gathered');
+        console.log(response);
+        setContacts(response);
+        console.log('set contacts');
+        console.log(contacts);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
-  renderContacts();
-
+  console.log(window.isSecureContext);
+  console.log('Navigator');
+  console.log(navigator);
   return (
     <Fragment>
       <Header online={isOnLine} />
       <div>
-        {!contactsSupported && (
-          <p id="notSupported">
-            <b>Sorry!</b> This browser doesn't support the Contact
-              Picker API, which required for this demo. Try enabling the
-            <code>#enable-experimental-web-platform-features</code> in
-              chrome://flags and try again.
-          </p>
-
-        )}
+        {contactsSupported ? (
+          <button type="button" onClick={gatherContacts}>
+            Obtener contactos
+          </button>
+        ) : (
+            <p id="notSupported">
+              <b>Sorry!</b>This browser doesn't support the Contact Picker API, which required for this demo.
+            Try enabling the <code>#enable-experimental-web-platform-features</code> in chrome://flags and try again.
+            </p>
+          )}
         <CurrentPrice />
         <History />
         <Snackbar online={isOnLine} />
